a1e92f3f8b5817fb42f07e1ec1e8d77dbeb77533
hanxinyan <metatron96@126.com>
2020-05-15 14:05:39 +0800

    创建LeetCode刷题记录仓库

---
+++ topic/二叉树/二叉树的层序遍历/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution
+{
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>> res;
+        if(!root) 
+            return res;
+        queue<TreeNode*> qu;
+        qu.push(root);
+        while(!qu.empty())
+        {
+            vector<int> tmp;
+            int len = qu.size();
+            for(int i = 0; i < len; ++i)
+            {
+                TreeNode* node = qu.front();
+                qu.pop();
+                tmp.push_back(node->val);
+                if(node->left)
+                    qu.push(node->left);
+                if(node->right)
+                    qu.push(node->right);
+            }
+            res.push_back(tmp);
+        }
+        return res;
+    }
+
+};
+
+class BiTree
+{
+public:
+    void create_BTree(TreeNode *& node)
+    {
+        string data;
+        cout << "请输入结点：" << endl;
+        cin >> data;
+        string check = {"null"};
+        //结点不存在
+        if(data == check)
+        {
+            node = NULL;
+            cout << "结点不存在" << endl; 
+        }
+        else
+        {
+            node = new TreeNode(stoi(data));
+            cout << "当前结点为" << node->val << endl; 
+            create_BTree(node->left);
+            cout << "当前结点为" << node->val << endl; 
+            create_BTree(node->right);
+        }
+    }
+
+    void Delete(TreeNode *& node)
+    {
+        if(node)
+        {
+            Delete(node->left);
+            Delete(node->right);
+            delete node;
+        }
+    }
+
+};
+
+
+int main()
+{
+    TreeNode * root;
+    BiTree biTree;
+    biTree.create_BTree(root);
+
+    Solution solution;
+    solution.levelOrder(root);
+
+    biTree.Delete(root);
+
+}
---
+++ topic/哈稀表/和为K的子数组/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    /**
+     * 暴力求解
+     */
+    // int subarraySum(vector<int> &nums, int k)
+    // {
+    //     vector<vector<int>> result;
+        
+    //     for(auto iter = nums.begin(); iter != nums.end(); ++iter)
+    //     {
+    //         vector<int> subarray{*iter};
+    //         int subSum = *iter;
+    //         if(subSum == k)
+    //         {
+    //             result.push_back(subarray);
+    //         }                
+    //         for(auto it = iter + 1; it != nums.end(); ++it)
+    //         {
+    //             subarray.push_back(*it);
+    //             subSum += *it;
+    //             if(subSum == k)
+    //             {
+    //                 result.push_back(subarray);
+    //                 continue;
+    //             }
+    //         }                
+    //     }
+
+    //     return result.size();
+    // }
+
+    
+    /**
+     * 暴力求解， 不使用容器
+     */
+    // int subarraySum(vector<int> &nums, int k)
+    // {
+    //     int size = nums.size(), res = 0;
+    //     for (int i = 0; i < size; ++i) {
+    //         int sum = 0;
+    //         for (int j = i; j < size; ++j) {
+    //             sum += nums[j];
+    //             res += sum == k ? 1 : 0;
+    //         }
+    //     }
+    //     return res;
+    // }
+
+    /**
+     *  前缀和+哈稀表优化
+     */
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int count = 0;
+        int preSum = 0;
+        for(auto &i : nums)
+        {
+            preSum += i;
+            if(mp.find(preSum - k) != mp.end()) count += mp[preSum - k];
+            ++mp[preSum];
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    vector<int> nums{1,1,1};
+
+    Solution solution;
+    int k = solution.subarraySum(nums, 2);
+
+    return 0;
+}
