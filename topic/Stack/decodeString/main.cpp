9f766b190793f9dd8d19effe94e4162b513ac12d
hanxinyan <metatron96@126.com>
2020-06-02 17:28:00 +0800

    新增空库

--- topic/Stack/decodeString/main.cpp
+++ topic/Stack/decodeString/main.cpp
@@ -6,16 +6,26 @@
 #include <queue>
 #include <string>
 #include <unordered_map>
+#include <stack>
 
 using namespace std;
 
 class Solution {
  public:
     string decodeString(string s) {
+        string ans;
+        stack<string> st;
+        
 
+        return ans;
     }
 };
 
 int main() {
+    string s{"3[a2[c]]"};
+    Solution solution;
+    string result = solution.decodeString(s);
+    cout << result << endl;
+
     return 0;
 }
