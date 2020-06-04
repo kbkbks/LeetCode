e2f8c5357dda35036f316cac7553072d0f32ae26
hanxinyan <metatron96@126.com>
2020-06-05 00:03:55 +0800

    新增除自身以外数组的乘积

--- topic/Array/productExceptSelf/main.cpp
+++ topic/Array/productExceptSelf/main.cpp
@@ -13,19 +13,69 @@ using namespace std;
 
 class Solution {
  public:
-    // 逻辑运算符短路性质
-    int sumNums(int n) {
-        n && (n += sumNums(n - 1));
+    // // 暴力求解
+    // vector<int> productExceptSelf(vector<int>& nums) {
+    //     vector<int> ans(nums.size(), 1);
 
-        return n;
+    //     for (int i = 0; i < nums.size(); ++i) {
+    //         for (int j = 0; j < nums.size(); ++j) {
+    //             if (j == i) continue;
+    //             ans[i] *= nums[j];
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    // // 左右乘积列表
+    // vector<int> productExceptSelf(vector<int>& nums) {
+    //     int length = nums.size();
+    //     // 左右两侧的乘积列表
+    //     vector<int> L(length, 1);
+    //     vector<int> R(length, 1);
+    //     vector<int> ans(length, 1);
+
+    //     // 计算左右两侧乘积列表LR的值
+    //     for (int i = 1; i < length; ++i) {
+    //         L[i] = L[i - 1] * nums[i - 1];
+    //     }
+    //     for (int i = length - 2; i >= 0; --i) {
+    //         R[i] = R[i + 1] * nums[i + 1];
+    //     }
+    //     // 左右两边乘积则为输出
+    //     for (int i = 0; i < length; ++i) {
+    //         ans[i] = L[i] * R[i];
+    //     }
+
+    //     return ans;
+    // }
+
+    // 空间复杂度O(1)
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int length = nums.size();
+        vector<int> ans(length, 1);
+        // ans表示左侧所有元素的乘积
+        for (int i = 1; i < length; ++i) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        // R为右侧所有元素的乘积
+        int R = 1;
+        for (int i = length - 1; i >= 0; --i) {
+            // 左右两边乘积则为输出
+            ans[i] = ans[i] * R;
+            // 动态更新下一个R
+            R *= nums[i];
+        }
+
+        return ans;
     }
 };
 
 int main() {
-    int n = 5;
+    vector<int> nums{1, 2, 3, 4};
     Solution solution;
-    int result = solution.sumNums(n);
-    cout << result << endl;
+    vector<int> result = solution.productExceptSelf(nums);
 
     return 0;
 }
