c9b32c11662f295a03662552f130751bcbd74351
hanxinyan <metatron96@126.com>
2020-06-30 10:27:06 +0800

    反转链表，新增createList

--- topic/LinkedList/reverseList/main.cpp
+++ topic/LinkedList/reverseList/main.cpp
@@ -21,11 +21,20 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    explicit ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
  public:
+    void createList(ListNode * head) {
+        ListNode * p = head;
+        for (int i = 2; i <= 5; ++i) {
+            ListNode * pNewNode = new ListNode(i);
+            p->next = pNewNode;
+            p = pNewNode;
+        }
+    }
+
     ListNode* reverseList(ListNode* head) {
         ListNode * pre = NULL;
         ListNode * cur = head;
@@ -41,5 +50,10 @@ class Solution {
 };
 
 int main() {
+    Solution solution;
+    ListNode * head = new ListNode(1);
+    solution.createList(head);
+    ListNode * result = solution.reverseList(head);
+
     return 0;
 }
