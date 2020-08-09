76a91c0d1647ae041b09fa3591c2e83366574a6c
hanxinyan <metatron96@126.com>
2020-08-09 22:54:17 +0800

    合并两个有序链表，增加递归法

--- topic/LinkedList/mergeTwoLists/main.cpp
+++ topic/LinkedList/mergeTwoLists/main.cpp
@@ -20,23 +20,42 @@ struct ListNode {
 
 class Solution {
  public:
+    /**
+     * 迭代法
+     */
+    // ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    //     ListNode head(0);
+    //     ListNode * cur = &head;
+    //     while (l1 && l2) {
+    //         if (l1->val <= l2->val) {
+    //             cur->next = l1;
+    //             l1 = l1->next;
+    //             cur = cur->next;
+    //         } else {
+    //             cur->next = l2;
+    //             l2 = l2->next;
+    //             cur = cur->next;
+    //         }
+    //     }
+    //     if (l1) cur->next = l1;
+    //     if (l2) cur->next = l2;
+    //     return head.next;
+    // }
+
+    /**
+     * 递归法
+     */
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode head(0);
-        ListNode * cur = &head;
-        while (l1 && l2) {
-            if (l1->val <= l2->val) {
-                cur->next = l1;
-                l1 = l1->next;
-                cur = cur->next;
-            } else {
-                cur->next = l2;
-                l2 = l2->next;
-                cur = cur->next;
-            }
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
-        if (l1) cur->next = l1;
-        if (l2) cur->next = l2;
-        return head.next;
     }
 };
 
